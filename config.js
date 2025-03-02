51878ec3ed59ad8b046f160ee843158efee4cb7a
ant <email.ansh.on@gmail.com>
2025-03-02 11:34:56 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-  baseUrl: typeof window !== 'undefined' ? window.location.origin : process.env.BASE_URL || 'http://localhost:3000'
+  baseUrl: process.env.BASE_URL || 'http://localhost:3000'
 };
 
-export default config;
+module.exports = config;
